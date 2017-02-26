464037efce99d977be2efea9fbf5ed6d73563812
minhhuyen93 <nmhuyen.hcmus@gmail.com>
2017-02-26 14:12:23 +0700

    crefactor html

--- src/securityModule.ts
+++ src/securityModule.ts
@@ -4,15 +4,18 @@ import { FormsModule } from "@angular/forms";
 import { LayoutPage } from "./defaultPage";
 import { SecurityRoutes } from "./securityRoutes";
 import { Categories } from "./category";
-import { AddOrEditCategory} from "./addOrEditCategory";
-import {CategoryService} from "./categoryService";
-import {RedColor} from "./redColor";
-import {CategorySummary} from "./categorySummary";
-import {FormInput} from "./formInput";
+import { AddOrEditCategory } from "./addOrEditCategory";
+import { CategoryService } from "./categoryService";
+import { RedColor } from "./redColor";
+import { CategorySummary } from "./categorySummary";
+import { FormInput } from "./formInput";
+import { Page } from "./page";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 @NgModule({
     imports: [BrowserModule, FormsModule, SecurityRoutes],
-    declarations: [LayoutPage, Categories, AddOrEditCategory,RedColor, CategorySummary, FormInput], 
-    providers:[CategoryService],
-    bootstrap: [LayoutPage]
+    declarations: [LayoutPage, Categories, AddOrEditCategory, RedColor, CategorySummary, FormInput, Page],
+    providers: [CategoryService],
+    bootstrap: [LayoutPage],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class SecurityModule { }
